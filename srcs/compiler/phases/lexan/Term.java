4056d5da106c367908310df1bf54b4d0dce819e8
Žan Palčič <palcic.zan@gmail.com>
2018-03-03 10:19:56 +0100

    Initial commit

---
+++ srcs/compiler/phases/lexan/Term.java
@@ -0,0 +1,42 @@
+package compiler.phases.lexan;
+
+/**
+ * CFG terminals, i.e., tokens.
+ * 
+ * @author sliva
+ *
+ */
+public enum Term {
+
+	EOF,
+	
+	IOR,
+	XOR,
+
+	AND,
+
+	EQU, NEQ, LTH, GTH, LEQ, GEQ,
+
+	ADD, SUB, MUL, DIV, MOD,
+
+	NOT,
+
+	MEM, VAL, // $ and @
+	
+	NEW, DEL,
+
+	ASSIGN,
+
+	COLON, COMMA, DOT, SEMIC,
+
+	LBRACE, RBRACE, LBRACKET, RBRACKET, LPARENTHESIS, RPARENTHESIS,
+
+	VOIDCONST, BOOLCONST, CHARCONST, INTCONST, PTRCONST,
+
+	VOID, BOOL, CHAR, INT, PTR, ARR, REC,
+
+	DO, ELSE, END, FUN, IF, THEN, TYP, VAR, WHERE, WHILE,
+
+	IDENTIFIER,
+
+}
